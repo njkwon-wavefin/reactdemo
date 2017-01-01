c8889e8290554424d4d521d1be529a5e712eb8e7
namkwon <namjookwon76@gmail.com>
2017-01-01 18:43:45 -0500

    basic files using react start script

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React , { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render(){
+    return(<div className="Header">{this.props.title}</div>);
+  }
+}
+
+export default Header;

d2e4e8255b1fcdfc83a6b91be7c75cf0b783e980
namkwon <namjookwon76@gmail.com>
2017-01-26 10:38:23 -0500

    changed router to hashhistory for github build

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from 'react-router';
 import {MovieContainer, MovieDetail} from './containers';
 import {DisplayMsg} from './components';
 
 ReactDOM.render(
-  <Router history={browserHistory} >
+  <Router history={hashHistory} >
     <Route path="/" component={App}>
       <IndexRoute component={MovieContainer} />
       <Route path="/movie/:id" component={MovieDetail} />

25bdc91a53af5463beb87f19138d2cb040920e73
namkwon <namjookwon76@gmail.com>
2017-12-27 18:23:37 -0500

    add styled component

--- src/components/CastList.jsx
+++ src/components/CastList.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Cast from './Cast';
 import {Link} from 'react-router';
 import { Col } from 'react-bootstrap';
+import styled from 'styled-components';
 
 export default function CastList({data}) {
+  const StyledLink = styled(Link)`
+    &:hover {
+      text-decoration:none;
+    }
+  `;
     var casts = data.map(function(cast) {
       if(cast.profile_path != null) {
         return(
           <Col xs={4} sm={3} md={2} key={cast.id} >
-            <Link to={'/star/'+cast.id} ><Cast cast={cast} /></Link>
+            <StyledLink to={'/star/'+cast.id} ><Cast cast={cast} /></StyledLink>
           </Col>
         );
      }

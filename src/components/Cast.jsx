81ecca14c323ba489272a08d692f8c6615829350
Mujtaba Al-Tameemi <mujtaba.altameemi@gmail.com>
2019-07-29 13:42:13 +0300

    fix poster images (cast images) by setting the proper size

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Thumbnail } from 'react-bootstrap/lib';
-import { URL_IMG, IMG_SIZE_SMALL } from '../const';
+import { URL_IMG, POSTER_SIZE_SMALL } from '../const';
 
 export default function Cast({cast}) {
   return (
-    <Thumbnail src={URL_IMG+IMG_SIZE_SMALL+cast.profile_path} alt={cast.name} >
+    <Thumbnail src={URL_IMG+POSTER_SIZE_SMALL+cast.profile_path} alt={cast.name} >
       <p>{cast.name}</p>
     </Thumbnail>
   );
--- src/const.js
+++ src/const.js
@@ -9,6 +9,7 @@ export const URL_VIDEO = '/videos';
 export const IMG_SIZE_XSMALL = 'w45/';
 export const IMG_SIZE_SMALL = 'w150/';
 export const IMG_SIZE_LARGE = 'w342/';
+export const POSTER_SIZE_SMALL = 'w154/';
 export const CAST_MAX_NUM = 5;
 export const TRAILER_MAX_NUM = 3;
 export const API_KEY = '?api_key=4d4ed145d3584846f5922b6a467e1f85';
